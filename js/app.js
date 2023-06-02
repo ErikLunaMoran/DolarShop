259685f236d7bb80ae294a50f0ceb4d9ec0d8b8f
Erik Luna Morán <eriklunamoran@gmail.com>
2023-06-02 20:53:17 -0300

    lógica terminada

--- js/app.js
+++ js/app.js
@@ -7,63 +7,70 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-/* Recorrido de Productos */
+/* funcion asincrona */
+const getProducts = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  {
-    content.innerHTML = `<div class="card-header">
-  <img src="${product.img}">
-  </div>
-
-  <div class="card-body">
-  <h3 class="card-title">${product.titulo}</h3>
-  <p class="card-description">$ ${product.precio}</p>
-  </div>
-  `;
-  }
-
-  /* <a href="#" id="comprar" class="btn">Comprar</a> */
-  shopContent.append(content);
-
-  /* Botón */
-  let comprar = document.createElement("button");
-  comprar.innerText = "Comprar";
-  comprar.className = "btn";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () => {
-    /* Checkeo que el producto no esté repetido */
-    const repeat = carrito.some(
-      (repeatProduct) => repeatProduct.id === product.id
-    );
-    console.log(repeat);
-
-    /* si se repite detecto el id y sumo +1 a cantidad*/
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
-        }
-      });
-    } else {
-      /* Pusheo producto en el carrito */
-      carrito.push({
-        id: product.id,
-        img: product.img,
-        titulo: product.titulo,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      });
-      console.log(carrito);
-      console.log(carrito.length);
-      carritoCounter();
-      saveLocal();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    {
+      content.innerHTML = `<div class="card-header">
+    <img src="${product.img}">
+    </div>
+  
+    <div class="card-body">
+    <h3 class="card-title">${product.titulo}</h3>
+    <p class="card-description">$ ${product.precio}</p>
+    </div>
+    `;
     }
+
+    /* <a href="#" id="comprar" class="btn">Comprar</a> */
+    shopContent.append(content);
+
+    /* Botón */
+    let comprar = document.createElement("button");
+    comprar.innerText = "Comprar";
+    comprar.className = "btn";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () => {
+      /* Checkeo que el producto no esté repetido */
+      const repeat = carrito.some(
+        (repeatProduct) => repeatProduct.id === product.id
+      );
+      console.log(repeat);
+
+      /* si se repite detecto el id y sumo +1 a cantidad*/
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        });
+      } else {
+        /* Pusheo producto en el carrito */
+        carrito.push({
+          id: product.id,
+          img: product.img,
+          titulo: product.titulo,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        saveLocal();
+      }
+    });
   });
-});
+};
+
+getProducts();
+/* Recorrido de Productos */
 
 /* Local Storage */
 /* seteo items  a guardar*/
